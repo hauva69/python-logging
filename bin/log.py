91546f417f8dac3d9a1eb61b6b1ce627666a0b5f
Ari Makela <ari.makela@iki.fi>
2022-10-10 20:35:00 +0300

    _log2file()

--- bin/log.py
+++ bin/log.py
@@ -11,6 +11,7 @@ def _very_simple():
 def _log2file():
     _clear_log_config()
     _fn = 'example.log'
+    # encoding was introduced in Python 3.9.
     logging.basicConfig(filename=_fn, encoding='utf-8', level=logging.DEBUG)
     logging.debug('this message goes to the log file %s', _fn)
     logging.info('this is info')
